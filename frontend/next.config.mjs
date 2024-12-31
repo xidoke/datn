b70ad1d166032afb8e34098d683e5937e2d3b636
Do Pham Dinh <do.pdwork@gmail.com>
2024-12-31 21:39:57 +0700

    fix tiep :)

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "xidoke.id.vn",
-        port: "443",
+        port: "",
         pathname: "/uploads/**",
       },
     ],
